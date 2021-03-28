3999fa4e91db8ebf15adfdbccfc83905a59d55a5
Tony5t4rk <978539910@qq.com>
2021-03-28 12:27:44 +0800

    update daily

---
+++ daily/2021/03/2021-3-28(173).py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+
+    def next(self) -> int:
+        ret = self.stack.pop()
+        cur = ret.right
+        while cur:
+            self.stack.append(cur)
+            cur = cur.left
+        return ret.val
+
+
+    def hasNext(self) -> bool:
+        if self.stack:
+            return True
+        return False
+
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()

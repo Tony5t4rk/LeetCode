acc67c619cf0ae1058a0730e0a5973f5b8a905d8
Tony5t4rk <978539910@qq.com>
2021-04-30 12:08:25 +0800

    update daily

--- daily/2021/04/2021-4-30(137. 只出现一次的数字 II).py
+++ daily/2021/04/2021-4-30(137. 只出现一次的数字 II).py
@@ -1,11 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        cnt = {}
-        for num in nums:
-            if num in cnt:
-                cnt[num] += 1
-            else:
-                cnt[num] = 1
-            if cnt[num] == 3:
-                del cnt[num]
-        return cnt.popitem()[0]
+        cnt = collections.Counter(nums)
+        ans = [num for num, occ in cnt.items() if occ == 1][0]
+        return ans
